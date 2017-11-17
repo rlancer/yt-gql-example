d5b2873e5a6abfdde141068132dda19ba60314d3
Robert Lancer <rob@collaborizm.com>
2017-11-17 13:34:35 -0500

    search results

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {search: ''}
+  }
+
   render() {
     return (
-      <div >
-
+      <div>
+        <h1>{this.state.search}</h1>
+        <input placeholder='Search' onChange={e => this.setState({search: e.target.value})} type='text'/>
+        <SearchResults/>
       </div>
     );
   }

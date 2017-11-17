12decb63dfd954eda9ced4f3dc2426ae21048c42
Robert Lancer <rob@collaborizm.com>
2017-11-17 00:45:14 -0500

    added client

--- index.mjs
+++ index.mjs
@@ -1,12 +1,15 @@
+import dotenv from 'dotenv'
+
 import gql from 'graphql'
 import graphqlHTTP from 'express-graphql'
 import express from 'express'
 import gapiToGraphQL from 'gapi-to-graphql'
 // Use any of the APIs included under the google_apis/ folder, or bring your own API descriptor
 import YouTubeAPI from 'gapi-to-graphql/google_apis/youtube-v3'
-
 import graphAddMiddleware from 'graphql-add-middleware'
 
+
+dotenv.config()
 const app = express()
 
 // need this separate assignment when using nodes experimental modules feature
@@ -16,32 +19,44 @@ const {GraphQLObjectType, GraphQLSchema} = gql
 const youtubeApi = gapiToGraphQL({gapiAsJsonSchema: YouTubeAPI, graphQLModule: gql})
 
 
-const ssss = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {...youtubeApi}
-  })
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'RootQueryType',
+        fields: {...youtubeApi}
+    })
 })
 
 
-graphAddMiddleware.addMiddleware(ssss, async function (root, args, context, info, next) {
-  // you can modify root, args, context, info
+const getPath = path => {
+    // eventually construct a full path here
+    const {prev, key} = path
+    return key
+}
 
 
-  console.log('roots')
+graphAddMiddleware.addMiddleware(schema, async function (root, args, context, info, next) {
+    // you can modify root, args, context, info
 
-  const result = await next()
-  // you can modify result
-  return result // you must return value
+    const path = getPath(info.path)
+
+    switch (path) {
+        case 'youtubeV3':
+            args.key = process.env.YOUTUBE_API
+            break
+    }
+
+
+    const result = await next()
+    // you can modify result
+    return result // you must return value
 })
-
 
 
 app.use('/graphql',
-  graphqlHTTP({
-    schema: ssss,
-    graphiql: true
-  })
+    graphqlHTTP({
+        schema: GraphQLSchema,
+        graphiql: true
+    })
 )
 
 

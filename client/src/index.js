0c6d6daf3cad08c794083c1603c2daf4fa3407ea
Robert Lancer <rob@collaborizm.com>
2018-01-08 23:08:54 -0500

    updated packages

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import {HttpLink} from 'apollo-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 
 const client = new ApolloClient({
-  link: new HttpLink({uri: 'http://localhost:4000/graphql'}),
+  link: new HttpLink({uri: `/graphql`}),
   cache: new InMemoryCache({
 
       dataIdFromObject: object => {
